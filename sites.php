74e70dfb6c0c6b9e2ab2db3608971c06d726609c
Djason irvinton SYLVAINCE <djasoirvinton.sylvaince@esih.edu>
2015-08-31 02:58:55 -0400

    version 2
    TiPeyizanPam

---
+++ sites.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Gallery page</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<!--
+Template 2029 Slide Wall
+http://www.tooplate.com/view/2029-slide-wall
+-->
+<link href="tooplate_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" type="text/css" href="css/jquery.lightbox-0.5.css" />    
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js" type="text/javascript"></script>
+<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
+    
+<!-- Arquivos utilizados pelo jQuery lightBox plugin -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.lightbox-0.5.js"></script>
+<link rel="stylesheet" type="text/css" href="css/jquery.lightbox-0.5.css" media="screen" />
+<!-- / fim dos arquivos utilizados pelo jQuery lightBox plugin -->
+
+<!-- Ativando o jQuery lightBox plugin -->
+<script type="text/javascript">
+$(function() {
+    $('#gallery a').lightBox();
+});
+</script>
+
+</head>
+<body>
+<select>
+<option><a href="fr.html">FR</a></option>
+<option><a href="us.html">US</a></option>
+<option><a href="ht.html">HT</a></option>
+<option><a href="es.html">ES</a></option>
+</select>
+<div id="tooplate_header">
+    <div id="site_title"><h1><a href="#">Ti Peyizan Pam</a></h1></div>
+    
+    <div id="tooplate_menu">
+        <ul>
+            <li><a href="design.html">Home</a></li>
+            <li><a href="#" class="current">Gallery</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="contact.html" class="last">Contact</a></li>
+        </ul>    	
+    </div> <!-- end of tooplate_menu -->
+    <div class="cleaner"></div>
+</div>
+
+<div id="tooplate_middle">
+    
+    <div id="slider">
+    <!--    <a href="#"><img src="images/slideshow/01.jpg" alt="slideshow 01" title="Morbi sit amet nulla vitae neque auctor malesuada sed ac ante." /></a>
+        <a href="#"><img src="images/slideshow/02.jpg" alt="slideshow 02" title="Sed consequat rutrum velit, sit amet ultricies est adipiscing molestie." /></a>
+        <a href="#"><img src="images/slideshow/04.jpg" alt="slideshow 04" title="In nec felis id magna dignissim rhoncus sed in nulla." /></a> -->
+		 <a href="#"><img src="images/slideshow/02.jpg" alt="slideshow 03" title="Fusce dignissim justo in odio aliquet luctus imperdiet dui molestie." /></a>  
+    </div>
+</div>
+
+    
+<div id="tooplate_main">
+    
+    <div class="col_w960">
+        	
+
+  </div>
+    
+    <div class="cleaner"></div>
+</div> <!-- end of main -->
+
+<div id="tooplate_footer_wrapper">
+	<div id="tooplate_footer">
+    	
+        Copyright © 2015 <a href="#">Company Name</a> - Design: <b><a href="http://www.tipeyizanpam.com"><span class="orange">Android</span><span class="green">pam</span></a></b>
+
+    
+    </div> <!-- end of footer wrapper -->
+</div> <!-- end of footer -->
+        
+</body>
+</html>
